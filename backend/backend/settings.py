31946ca71ed5471e6fe5f4be3e6e86f1b8c2eea3
KamilObroslak <kamilobroslak1@gmail.com>
2023-07-29 23:25:03 +0200

    modify database in settings.py for AWS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 #
 # DATABASES = {
 #     "default": {
@@ -95,6 +95,18 @@ DATABASES = {
 #         "PORT": 5432,  # default postgres port
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "djangocafe",
+        "USER": "admin",
+        "PASSWORD": "password",
+        "HOST": "localhost",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
